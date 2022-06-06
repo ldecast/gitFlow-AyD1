337d2e8a33b6c36b440e299a554a0d77d4376411
ldecast <3006240180101@ingenieria.usac.edu.gt>
2022-06-06 08:16:03 -0600

    Se agregó funcionalidad suma
    Usa puerto 3000

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+PORT = 3000
+
+
+@app.route('/sumar', methods=['POST'])
+def Suma():
+    request_data = request.get_json()
+    return sumar(request_data['num1'], request_data['num2'])
+
+
+def sumar(num1: int, num2: int):
+    result = num1+num2
+    return jsonify({'Resultado': result})
+
+
+if __name__ == '__main__':
+    # run app in debug mode on port 3000
+    app.run(debug=True, port=PORT)
